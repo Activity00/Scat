24bd4a7e54c695d9cfa2df2abf0df6e5bd8df893
Activity00 <1032662429@qq.com>
2018-04-24 10:24:07 +0800

    FIX: fix log repeat problem

--- scat/utils/logutil.py
+++ scat/utils/logutil.py
@@ -6,17 +6,29 @@
 """
 import logging
 
+import tornado.log
 from tornado.log import LogFormatter
 
 
+# class ScatLogUtil:
+#     def __init__(self):
+#         self.logger = logging.getLogger('Scat_log')
+#         self.logger.setLevel(logging.DEBUG)
+#         formatter = LogFormatter('%(color)s[%(asctime)s %(levelname)s]:%(end_color)s %(message)s')
+#         console_handler = logging.StreamHandler()
+#         console_handler.formatter = formatter
+#         self.logger.addHandler(console_handler)
+#
+#     def get_logger(self):
+#         return self.logger
+
 class ScatLogUtil:
     def __init__(self):
-        self.logger = logging.getLogger('Scat_log')
+        self.logger = logging.getLogger()
         self.logger.setLevel(logging.DEBUG)
         formatter = LogFormatter('%(color)s[%(asctime)s %(levelname)s]:%(end_color)s %(message)s')
-        console_handler = logging.StreamHandler()
-        console_handler.formatter = formatter
-        self.logger.addHandler(console_handler)
+        tornado.log.enable_pretty_logging(logger=self.logger)
+        self.logger.handlers[0].setFormatter(formatter)
 
     def get_logger(self):
         return self.logger
