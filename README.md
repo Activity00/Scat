7d8ad7bf2d983bf0ad91d576724c25a5769eeef0
Activity00 <1032662429@qq.com>
2018-05-21 20:21:24 +0800

    ADD: add templates for create project

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Scat
 
-# 
+Scat want to be a fast, scalable game server framework for tornado.
+It provides the basic development framework and many related components, including libraries and tools. 
+Scat is also suitable for real-time web applications with tornado.
+
+# Features
+> Fast, scalable
+
+> Easy
+
+> Extensible
+
+# Basic
+
+1. install the package
+> pip install scat
+
+
+2. create your first project
+> scat createproject mydemo
+
+3. ccreate a game server app for your demo
+
+4. add basic config in your settings.py
+~~~
+
+~~~
--- scat/commands.py
+++ scat/commands.py
@@ -4,17 +4,45 @@
 @author: 武明辉 
 @time: 2018/1/20 19:14
 """
-import fire
+import string
 
-from scat.distributed.master import MASTER_SERVER_MODE, SINGLE_SERVER_MODE, MULTI_SERVER_MODE
-from scat.server import ScatServer
+import fire
+import os
+import shutil
+
+TEMPLATES_TO_RENDER = (
+    ('scrapy.cfg',),
+    ('${project_name}', 'settings.py.tmpl'),
+    ('${project_name}', 'items.py.tmpl'),
+    ('${project_name}', 'pipelines.py.tmpl'),
+    ('${project_name}', 'middlewares.py.tmpl'),
+)
 
 
 class Commands:
-    def createproject(self, name, path='.'):
-        return 'create project {} path: {}'.format(name, path)
+    def createproject(self, name, path='.', template_dir=None):
+        import scat
+        from os.path import join, abspath
+
+        # template_base_dir = template_dir or join(scat.__path__[0], 'templates')
+        # self._copytree(template_base_dir, abspath(path))
+        #
+        # shutil.move(join(path, 'module'), join(path, name))
+        # for paths in TEMPLATES_TO_RENDER:
+        #     path = join(*paths)
+        #     tplfile = join(path, string.Template(path).substitute(project_name=project_name))
+        #     render_templatefile(tplfile, project_name=project_name,
+        #                         ProjectName=string_camelcase(project_name))
+        # print("New Scrapy project %r, using template directory %r, created in:" % \
+        #       (project_name, self.templates_dir))
+        # print("    %s\n" % abspath(project_dir))
+        # print("You can start your first spider with:")
+        # print("    cd %s" % project_dir)
+        # print("    scrapy genspider example example.com")
+        # return 'create project {} path: {}'.format(name, path)
 
     def runserver(self, server_name='', mode=''):
+        from scat.distributed.master import MASTER_SERVER_MODE, SINGLE_SERVER_MODE, MULTI_SERVER_MODE
         from scat.distributed.master import Master
         if mode == "single":
             if server_name == "master":
@@ -28,11 +56,29 @@ class Commands:
         master.start(server_name, mode)
 
     def start_server(self, server_name):
+        from scat.server import ScatServer
         server = ScatServer(server_name)
         server.start()
 
     def __str__(self):
         return 'Python manage.py COMMANDNAME --paras'
+
+    def _copytree(self, src, dst):
+        names = os.listdir(src)
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+
+        for name in names:
+            if name.endswith('.pyc') or name.endswith('.git'):
+                continue
+
+            src_name = os.path.join(src, name)
+            dst_name = os.path.join(dst, name)
+            if os.path.isdir(src_name):
+                self._copytree(src_name, dst_name)
+            else:
+                shutil.copy2(src_name, dst_name)
+        shutil.copystat(src, dst)
 
 
 def execute_from_command_line():
