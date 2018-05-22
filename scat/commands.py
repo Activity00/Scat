b3d0b96d676b21b6feb49b8e60f0346915e52171
Activity00 <1032662429@qq.com>
2018-05-22 15:48:56 +0800

    ADD: create a project command

--- scat/commands.py
+++ scat/commands.py
@@ -10,36 +10,63 @@ import fire
 import os
 import shutil
 
+import re
+
 TEMPLATES_TO_RENDER = (
-    ('scrapy.cfg',),
-    ('${project_name}', 'settings.py.tmpl'),
-    ('${project_name}', 'items.py.tmpl'),
-    ('${project_name}', 'pipelines.py.tmpl'),
-    ('${project_name}', 'middlewares.py.tmpl'),
+    ('manage.py.tmpl',),
+    ('start_servers.py.tmpl', ),
+    # ('${project_name}', 'settings.py.tmpl'),
 )
 
 
+def render_templatefile(path, **kwargs):
+    with open(path, 'rb') as fp:
+        raw = fp.read().decode('utf8')
+
+    content = string.Template(raw).substitute(**kwargs)
+
+    render_path = path[:-len('.tmpl')] if path.endswith('.tmpl') else path
+    with open(render_path, 'wb') as fp:
+        fp.write(content.encode('utf8'))
+    if path.endswith('.tmpl'):
+        os.remove(path)
+
+
+CAMELCASE_INVALID_CHARS = re.compile('[^a-zA-Z\d]')
+
+
+def string_camelcase(string):
+    """ Convert a word  to its CamelCase version and remove invalid chars
+
+    >>> string_camelcase('lost-pound')
+    'LostPound'
+
+    >>> string_camelcase('missing_images')
+    'MissingImages'
+
+    """
+    return CAMELCASE_INVALID_CHARS.sub('', string.title())
+
+
 class Commands:
-    def createproject(self, name, path='.', template_dir=None):
+    def createproject(self, project_name, project_dir='.', template_dir=None):
         import scat
         from os.path import join, abspath
 
-        # template_base_dir = template_dir or join(scat.__path__[0], 'templates')
-        # self._copytree(template_base_dir, abspath(path))
-        #
-        # shutil.move(join(path, 'module'), join(path, name))
-        # for paths in TEMPLATES_TO_RENDER:
-        #     path = join(*paths)
-        #     tplfile = join(path, string.Template(path).substitute(project_name=project_name))
-        #     render_templatefile(tplfile, project_name=project_name,
-        #                         ProjectName=string_camelcase(project_name))
-        # print("New Scrapy project %r, using template directory %r, created in:" % \
-        #       (project_name, self.templates_dir))
-        # print("    %s\n" % abspath(project_dir))
-        # print("You can start your first spider with:")
-        # print("    cd %s" % project_dir)
-        # print("    scrapy genspider example example.com")
-        # return 'create project {} path: {}'.format(name, path)
+        template_base_dir = template_dir or join(scat.__path__[0], 'templates')
+        self._copytree(template_base_dir, abspath(project_dir))
+        shutil.move(join(project_dir, 'module'), join(project_dir, project_name))
+
+        for paths in TEMPLATES_TO_RENDER:
+            path = join(*paths)
+            tplfile = join(project_dir, string.Template(path).substitute(project_name=project_name))
+            render_templatefile(tplfile, project_name=project_name, ProjectName=string_camelcase(project_name))
+
+        print("New Scat project %r, using template directory %r, created in:" % (project_name, template_base_dir))
+        print("    %s\n" % abspath(project_dir))
+        print("You can start your first app with:")
+        print("    cd %s" % project_dir)
+        print("    scat startapp example")
 
     def runserver(self, server_name='', mode=''):
         from scat.distributed.master import MASTER_SERVER_MODE, SINGLE_SERVER_MODE, MULTI_SERVER_MODE
